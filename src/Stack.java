77c4bccf622a615bd79e9f2f0e0f5df64f3518b6
SebJoosten <sebjoosten@gmail.com>
2025-04-29 18:33:15 +1200

    The stack is compleat time to test it

--- src/Stack.java
+++ src/Stack.java
@@ -3,15 +3,114 @@
  */
 public class Stack {
 
+    private node root;
+    boolean debugPrint = true;
 
 
+    /**
+     * Used to push to the top of the stack
+     * @param data - The data in the form of a string to add to the top
+     */
+    public void push(String data){
+
+        if(data == null){
+            System.out.println("Nothing to push");
+            return;
+        }
+
+        node top = new node(data,root);
+        root = top;
+
+    }
+
+    /**
+     * Pop something off the top of this stack
+     * @return - The string in the stack or empty string
+     */
+    public String pop(){
+
+        // Root null check
+        if(root == null) {
+            if(debugPrint) System.out.println("ERROR Nothing to pop");
+            return "";
+        }
+
+        // Make sure we return something
+        String data = (root.data == null? "" : root.data);
+
+        // Data return error message
+        if(data.isEmpty()) {
+            if(debugPrint) System.out.println("ERROR No string to return pop");
+        }
+
+        root = root.next;
+        return data;
+
+    }
+
+    /**
+     * A method to look at the top of the stack without changing the stack
+     * @return - The String data at the top of the stack
+     */
+    public String peek(){
+
+        // Root null check
+        if(root == null) {
+            if(debugPrint) System.out.println("ERROR Nothing to peek");
+            return "";
+        }
+
+        // Make sure we return something
+        String data = (root.data == null? "" : root.data);
+
+        // Data return error message
+        if(data.isEmpty()) {
+            if(debugPrint) System.out.println("ERROR No string to return peek");
+        }
+
+        return data;
+
+    }
+
+    /**
+     * A method to check if the stack is empty
+     * @return - True if empty otherwise false
+     */
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    /**
+     * A method to count the items in this stack
+     * @return - the count of strings stored in this stack
+     */
+    public int length(){
+
+        // Root null check
+        if(root == null) {
+            if(debugPrint) System.out.println("ERROR Nothing to count");
+            return 0;
+        }
+
+        return root.count();
+
+    }
 
 
+    public void dump(){
 
+        // Null check
+        if (root == null) {
+            System.out.println("********************** Stack empty **********************");
+            return;
+        }
 
+        // Call the print to start the chain
+        System.out.println("********************** Stack  Dump **********************");
+        root.print();
+        System.out.println("********************* Stack Dump END ********************");
 
-
-
+    }
 
     /**
      * This is the Node for the linked list class
@@ -30,8 +129,11 @@ public class Stack {
          * @param next - The Next node in the linked list
          */
         public node(String data, node next){
-            this.data = data;
+
+            // Make sure data is never empty reduce null returns
+            this.data = (data == null) ? "" : data;
             this.next = next;
+
         }
 
         /**
@@ -39,7 +141,7 @@ public class Stack {
          * @return - The string held in this node
          */
         public String getData(){
-            return data;
+            return (data == null) ? "" : data;
         }
 
         /**
@@ -47,17 +149,55 @@ public class Stack {
          * @return - The next node after this one
          */
         public node getNext(){
+
+            if(next == null){
+                if (debugPrint) System.out.println("ERROR no next Node");
+            }
+
             return next;
         }
 
         /**
-         * This is to set the next node in the chain
-         * @param next - update the next node in this chain
+         * A method to cound the nodes in this list
+         * @return - the number of nodes in this list
          */
-        public void setNext(node next){
-            this.next = next;
+        public int count(){
+            return countRecursive(0);
         }
 
+        /**
+         * The recursive method used by the count method to cound the nodes in this list
+         * @param i - The count of nodes so far
+         * @return - the total number of nodes in the list
+         */
+        private int countRecursive(int i){
+
+            if (root.next == null){
+              return  i + 1;
+            }
+            return next.countRecursive(i+1);
+        }
+
+        /**
+         * This method calls the internal recursive method to print each node in this chain
+         */
+        public void print(){
+            printRecursive(1);
+        }
+
+        /**
+         * The recursive print method counts down the chain to make a nice print
+         * @param i - The current count we are up to
+         */
+        private void printRecursive(int i){
+
+            if (next == null) return;
+
+            System.out.println(i + ". -> " + data );
+
+            next.printRecursive(i+1);
+
+        }
 
     }
 
